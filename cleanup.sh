d9634f3042f57769e6b6204e612608bc904594f4
Colin Rundel <rundel@gmail.com>
2025-08-18 12:54:54 -0400

    Update

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+cd runner0/ && sudo ./svc.sh uninstall && cd ..
+cd runner1/ && sudo ./svc.sh uninstall && cd ..
+cd runner2/ && sudo ./svc.sh uninstall && cd ..
+cd runner3/ && sudo ./svc.sh uninstall && cd ..
+
+sudo rm -rf runner*
